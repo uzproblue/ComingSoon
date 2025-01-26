bbb0d8216cee815b74443013854d31b18a90c75a
firdavssaiz <firdavs.rakhmonov@saiz.io>
2025-01-26 12:36:26 +0500

    chnages

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,12 +13,12 @@ const IndexPage = () => (
       alt="Suzangaron Hotel"
       className="absolute w-full left-24 bottom-24 animate-blob2"
     />
-    <div className="relative z-10 py-6 space-y-16 lg:space-y-32 text-gray-500">
+    <div className="relative z-10 py-6 space-y-16 lg:space-y-32 text-gray-500 ">
       <div className="text-center space-y-10">
         <h3 className="font-light text-xl uppercase tracking-wider">
           Coming soon
         </h3>
-        <h1 className="text-5xl lg:text-7xl font-extrabold">
+        <h1 className="text-5xl lg:text-7xl font-extrabold text-yellow-500">
           Suzangaron Hotel
         </h1>
         <p className="text-xl lg:text-2xl tracking-wide mx-10 lg:max-w-xl text-white backdrop-contrast-100 lg:mx-auto">
